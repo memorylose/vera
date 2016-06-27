f0f7a2788c3ba09eb0e2123876ed2188f94b67df
memorylose <lucifering@126.com>
2016-06-27 20:36:44 +0800

    add password sha

---
+++ UM.Utility/EncryptUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace UM.Utility
+{
+    public class EncryptUtil
+    {
+        /// <summary>
+        /// Create string sha256 
+        /// </summary>
+        /// <param name="hashString"></param>
+        /// <returns></returns>
+        public static string CreateSHA256HashString(string password)
+        {
+            byte[] hashByte = System.Text.Encoding.UTF8.GetBytes(password);
+            SHA256 mySHA256 = SHA256Managed.Create();
+            return Convert.ToBase64String(mySHA256.ComputeHash(hashByte));
+        }
+
+        /// <summary>
+        /// Create random salt
+        /// </summary>
+        /// <param name="saltByteCount">36</param>
+        /// <returns></returns>
+        public static string GetRandomSalt(int saltByteCount)
+        {
+            RNGCryptoServiceProvider csprng = new RNGCryptoServiceProvider();
+            byte[] saltByte = new byte[saltByteCount];
+            csprng.GetBytes(saltByte);
+            return Convert.ToBase64String(saltByte);
+        }
+    }
+}
