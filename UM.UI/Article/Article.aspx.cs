e80938c96119f83a07c0190bedea21371145d1aa
memorylose <lucifering@126.com>
2016-07-01 20:57:19 +0800

    11

--- UM.UI/Article/Article.aspx.cs
+++ UM.UI/Article/Article.aspx.cs
@@ -18,9 +18,9 @@ namespace UM.UI.Article
             DataSet articleDs = userReg.ShowArticle();
             for (int i = 0; i < articleDs.Tables[0].Rows.Count; i++)
             {
-                Articlehtml += "" + "< div style = " + "\""+"height: 30px; text - align: center; font - size: 20px; margin - bottom: 20px; "+"\"" + ">" + "<br/>";
-                Articlehtml += "" + articleDs.Tables[0].Rows[i]["Title"].ToString() + "<br/>";
-                Articlehtml += "" + "</ div >";
+                Articlehtml += "<div style=\"height: 30px; text - align: center; font - size: 20px; margin - bottom: 20px; \">";
+                Articlehtml += articleDs.Tables[0].Rows[i]["Title"].ToString() + "<br/>";
+                Articlehtml += "</ div >";
             }
         }
     }
