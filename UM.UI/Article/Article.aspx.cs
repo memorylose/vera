5212c92f9ee6d1b76dbb30b46cc55e1c5f3d4447
memorylose <lucifering@126.com>
2016-07-01 20:00:47 +0800

    1

--- UM.UI/Article/Article.aspx.cs
+++ UM.UI/Article/Article.aspx.cs
@@ -11,13 +11,16 @@ namespace UM.UI.Article
 {
     public partial class Article : System.Web.UI.Page
     {
+        public string Articlehtml = "";
         protected void Page_Load(object sender, EventArgs e)
         {
             UserRegisterBusiness userReg = new UserRegisterBusiness();
             DataSet articleDs = userReg.ShowArticle();
-            string title = string.Empty;
             for (int i = 0; i < articleDs.Tables[0].Rows.Count; i++)
             {
+                Articlehtml += "" + "< div style = " + "\""+"height: 30px; text - align: center; font - size: 20px; margin - bottom: 20px; "+"\"" + ">" + "<br/>";
+                Articlehtml += "" + articleDs.Tables[0].Rows[i]["Title"].ToString() + "<br/>";
+                Articlehtml += "" + "</ div >";
             }
         }
     }
