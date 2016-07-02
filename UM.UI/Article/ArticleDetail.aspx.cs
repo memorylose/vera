cf19fc749949b5f934de6a3d34e1ee3db437282a
memorylose <lucifering@126.com>
2016-07-02 18:45:28 +0800

    21

--- UM.UI/Article/ArticleDetail.aspx.cs
+++ UM.UI/Article/ArticleDetail.aspx.cs
@@ -17,12 +17,9 @@ namespace UM.UI.Article
             UserRegisterBusiness userReg = new UserRegisterBusiness();
             DataSet contentDs = userReg.ArticleDetails();
             string content = contentDs.Tables[0].Rows[0]["Contents"].ToString();
-            for (int i = 0; i < contentDs.Tables[0].Rows.Count; i++)
-            {
-                Contenthtml += "<div style=\"height: 30px; width: 2200px; text - align: center; font - size: 13px; margin - bottom: 20px; float: left; \">";
-                Contenthtml += content;
-                Contenthtml += "</div>";
-            }
+            Contenthtml += "<div style=\"height: 30px; width: 2200px; text - align: center; font - size: 13px; margin - bottom: 20px; float: left; \">";
+            Contenthtml += content;
+            Contenthtml += "</div>";
         }
     }
 }
