3d3d28e29bef67d80fea0c74d5683d05cf4f3948
jiangyue1120 <jiangyue1120@126.com>
2016-07-05 21:44:13 +0800

    update

--- UM.DataAccess/UserRegisterData.cs
+++ UM.DataAccess/UserRegisterData.cs
@@ -120,7 +120,7 @@ namespace UM.DataAccess
 
         public DataSet ArticleDetails(int articleId)
         {
-            string sql = "select TypeName,Title,Contents,CreateDate from ArticleType inner join Articles on ArticleType.TypeId = Articles.TypeId where ArticleId=@articleId";
+            string sql = "select ArticleType.TypeName,Articles.Title,Articles.Contents,Articles.CreateDate from ArticleType inner join Articles on ArticleType.TypeId = Articles.TypeId where ArticleId=@articleId";
             SqlParameter[] sqlParam = {
                     new SqlParameter("@articleId",SqlDbType.Int)
             };
