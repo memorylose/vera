3101fa76cf646cbbef007329c07afe4ce830ad20
jiangyue1120 <jiangyue1120@126.com>
2016-08-06 15:54:07 +0800

    upload

--- UM.UI/Article/Article.aspx.cs
+++ UM.UI/Article/Article.aspx.cs
@@ -16,38 +16,77 @@ namespace UM.UI.Article
         protected void Page_Load(object sender, EventArgs e)
         {
             int articleId = 0;
+            string keyword = Request.QueryString["keyword"];
             UserRegisterBusiness userReg = new UserRegisterBusiness();
-            DataSet allarticleDs = userReg.ShowArticle();
-            for (int i = 0; i < allarticleDs.Tables[0].Rows.Count; i++)
+            if (keyword == null)
             {
-                articleId = Convert.ToInt32(allarticleDs.Tables[0].Rows[i]["ArticleId"]);
-                string title = allarticleDs.Tables[0].Rows[i]["Title"].ToString();
-                string crDate = allarticleDs.Tables[0].Rows[i]["CreateDate"].ToString();
-                string typeName = allarticleDs.Tables[0].Rows[i]["TypeName"].ToString();
-                string summary = allarticleDs.Tables[0].Rows[i]["Summary"].ToString();
-                if (summary == "")
+                DataSet allarticleDs = userReg.ShowArticle();
+                for (int i = 0; i < allarticleDs.Tables[0].Rows.Count; i++)
                 {
-                    string contents = allarticleDs.Tables[0].Rows[i]["Contents"].ToString();
-                    contents = System.Text.RegularExpressions.Regex.Replace(contents, RegexConstant.Htmlmark, "");
-                    if (contents.Length < 100)
+                    articleId = Convert.ToInt32(allarticleDs.Tables[0].Rows[i]["ArticleId"]);
+                    string title = allarticleDs.Tables[0].Rows[i]["Title"].ToString();
+                    string crDate = allarticleDs.Tables[0].Rows[i]["CreateDate"].ToString();
+                    string typeName = allarticleDs.Tables[0].Rows[i]["TypeName"].ToString();
+                    string summary = allarticleDs.Tables[0].Rows[i]["Summary"].ToString();
+                    if (summary == "")
                     {
-                        summary = contents;
+                        string contents = allarticleDs.Tables[0].Rows[i]["Contents"].ToString();
+                        contents = System.Text.RegularExpressions.Regex.Replace(contents, RegexConstant.Htmlmark, "");
+                        if (contents.Length < 100)
+                        {
+                            summary = contents;
+                        }
+                        else
+                        {
+                            summary = contents.Substring(0, 100);
+                            summary = summary.Substring(0, 99) + System.Text.RegularExpressions.Regex.Replace(summary.Substring(99, 1), RegexConstant.AlphaNumber, "");
+                            summary += "......";
+                        }
                     }
-                    else
-                    {
-                        summary = contents.Substring(0, 100);
-                        summary = summary.Substring(0, 99) + System.Text.RegularExpressions.Regex.Replace(summary.Substring(99, 1), RegexConstant.AlphaNumber, "");
-                        summary += "......";
-                    }
-                }
 
-                Articleshtml += "<div class=\"row\">";
-                Articleshtml += "<div class=\"col-md-12 content-title\"><a href=\"ArticleDetail.aspx?id=" + articleId + "\">" + title + "</a></div>";
-                Articleshtml += "<div class=\"col-md-12 content-date\"><span>" + crDate + "</span><span>-</span><span>" + typeName + "</span></div>";
-                Articleshtml += "<div class=\"col-md-12 content-t\">" + summary;
-                Articleshtml += "</div>";
-                Articleshtml += "</div>";
+                    Articleshtml += "<div class=\"row\">";
+                    Articleshtml += "<div class=\"col-md-12 content-title\"><a href=\"ArticleDetail.aspx?id=" + articleId + "\">" + title + "</a></div>";
+                    Articleshtml += "<div class=\"col-md-12 content-date\"><span>" + crDate + "</span><span>-</span><span>" + typeName + "</span></div>";
+                    Articleshtml += "<div class=\"col-md-12 content-t\">" + summary;
+                    Articleshtml += "</div>";
+                    Articleshtml += "</div>";
+                }
             }
+            else
+            {
+                DataSet searcharticleDs = userReg.SearchArticle(keyword);
+                for (int i = 0; i < searcharticleDs.Tables[0].Rows.Count; i++)
+                {
+                    string title = searcharticleDs.Tables[0].Rows[i]["Title"].ToString();
+                    string crDate = searcharticleDs.Tables[0].Rows[i]["CreateDate"].ToString();
+                    string typeName = searcharticleDs.Tables[0].Rows[i]["TypeName"].ToString();
+                    string summary = searcharticleDs.Tables[0].Rows[i]["Summary"].ToString();
+                    if (summary == "")
+                    {
+                        string contents = searcharticleDs.Tables[0].Rows[i]["Contents"].ToString();
+                        contents = System.Text.RegularExpressions.Regex.Replace(contents, RegexConstant.Htmlmark, "");
+                        if (contents.Length < 100)
+                        {
+                            summary = contents;
+                        }
+                        else
+                        {
+                            summary = contents.Substring(0, 100);
+                            summary = summary.Substring(0, 99) + System.Text.RegularExpressions.Regex.Replace(summary.Substring(99, 1), RegexConstant.AlphaNumber, "");
+                            summary += "......";
+                        }
+                    }
+
+                    Articleshtml += "<div class=\"row\">";
+                    Articleshtml += "<div class=\"col-md-12 content-title\"><a href=\"ArticleDetail.aspx?id=" + articleId + "\">" + title + "</a></div>";
+                    Articleshtml += "<div class=\"col-md-12 content-date\"><span>" + crDate + "</span><span>-</span><span>" + typeName + "</span></div>";
+                    Articleshtml += "<div class=\"col-md-12 content-t\">" + summary;
+                    Articleshtml += "</div>";
+                    Articleshtml += "</div>";
+                }
+            }
+
+
         }
     }
 }
--- UM.UI/UserControl/Todo.ascx.cs
+++ UM.UI/UserControl/Todo.ascx.cs
@@ -19,31 +19,8 @@ namespace UM.UI.UserControl
 
         protected void btnSearch_Click(object sender, EventArgs e)
         {
-            UserRegisterBusiness userReg = new UserRegisterBusiness();
-            DataSet searcharticleDs = userReg.SearchArticle(txtSearch.Value);
-            for (int i = 0; i < searcharticleDs.Tables[0].Rows.Count; i++)
-            {
-                string title = searcharticleDs.Tables[0].Rows[i]["Title"].ToString();
-                string crDate = searcharticleDs.Tables[0].Rows[i]["CreateDate"].ToString();
-                string typeName = searcharticleDs.Tables[0].Rows[i]["TypeName"].ToString();
-                string summary = searcharticleDs.Tables[0].Rows[i]["Summary"].ToString();
-                if (summary == "")
-                {
-                    string contents = searcharticleDs.Tables[0].Rows[i]["Contents"].ToString();
-                    contents = System.Text.RegularExpressions.Regex.Replace(contents, RegexConstant.Htmlmark, "");
-                    if (contents.Length < 100)
-                    {
-                        summary = contents;
-                    }
-                    else
-                    {
-                        summary = contents.Substring(0, 100);
-                        summary = summary.Substring(0, 99) + System.Text.RegularExpressions.Regex.Replace(summary.Substring(99, 1), RegexConstant.AlphaNumber, "");
-                        summary += "......";
-                    }
-                }
-            }
-            Response.Redirect("Article.aspx?keyword=" + txtSearch.Value);
+            string keyword = txtSearch.Value;
+            Response.Redirect("Article.aspx?keyword=" + keyword);
         }
     }
 }
