8be76c7517e2b0d36d24d404452af0c0425fa66a
jiangyue1120 <jiangyue1120@126.com>
2016-08-06 17:14:02 +0800

    update

--- UM.UI/Article/Article.aspx.cs
+++ UM.UI/Article/Article.aspx.cs
@@ -13,83 +13,56 @@ namespace UM.UI.Article
     public partial class Article : System.Web.UI.Page
     {
         public string Articleshtml = "";
+        int articleId = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
-            int articleId = 0;
             string keyword = Request.QueryString["keyword"];
             UserRegisterBusiness userReg = new UserRegisterBusiness();
             if (keyword == null)
             {
                 DataSet allarticleDs = userReg.ShowArticle();
-                for (int i = 0; i < allarticleDs.Tables[0].Rows.Count; i++)
-                {
-                    articleId = Convert.ToInt32(allarticleDs.Tables[0].Rows[i]["ArticleId"]);
-                    string title = allarticleDs.Tables[0].Rows[i]["Title"].ToString();
-                    string crDate = allarticleDs.Tables[0].Rows[i]["CreateDate"].ToString();
-                    string typeName = allarticleDs.Tables[0].Rows[i]["TypeName"].ToString();
-                    string summary = allarticleDs.Tables[0].Rows[i]["Summary"].ToString();
-                    if (summary == "")
-                    {
-                        string contents = allarticleDs.Tables[0].Rows[i]["Contents"].ToString();
-                        contents = System.Text.RegularExpressions.Regex.Replace(contents, RegexConstant.Htmlmark, "");
-                        //if (contents.Length < 100)
-                        //{
-                        //    summary = contents;
-                        //}
-                        //else
-                        //{
-                        //    summary = contents.Substring(0, 100);
-                        //    summary = summary.Substring(0, 99) + System.Text.RegularExpressions.Regex.Replace(summary.Substring(99, 1), RegexConstant.AlphaNumber, "");
-                        //    summary += "......";
-                        //}
-                        ShowSummary(contents, out summary);
-                    }
-                    ArticleHtml(articleId, title, crDate, typeName, summary);
-                }
+                ShowSummary(allarticleDs);
             }
             else
             {
                 DataSet searcharticleDs = userReg.SearchArticle(keyword);
-                for (int i = 0; i < searcharticleDs.Tables[0].Rows.Count; i++)
+                ShowSummary(searcharticleDs);
+            }
+        }
+
+        public void ShowSummary(DataSet ds)
+        {
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                string title = ds.Tables[0].Rows[i]["Title"].ToString();
+                string crDate = ds.Tables[0].Rows[i]["CreateDate"].ToString();
+                string typeName = ds.Tables[0].Rows[i]["TypeName"].ToString();
+                string summary = ds.Tables[0].Rows[i]["Summary"].ToString();
+                if (summary == "")
                 {
-                    string title = searcharticleDs.Tables[0].Rows[i]["Title"].ToString();
-                    string crDate = searcharticleDs.Tables[0].Rows[i]["CreateDate"].ToString();
-                    string typeName = searcharticleDs.Tables[0].Rows[i]["TypeName"].ToString();
-                    string summary = searcharticleDs.Tables[0].Rows[i]["Summary"].ToString();
-                    if (summary == "")
+                    string contents = ds.Tables[0].Rows[i]["Contents"].ToString();
+                    contents = System.Text.RegularExpressions.Regex.Replace(contents, RegexConstant.Htmlmark, "");
+                    if (contents.Length < 100)
                     {
-                        string contents = searcharticleDs.Tables[0].Rows[i]["Contents"].ToString();
-                        contents = System.Text.RegularExpressions.Regex.Replace(contents, RegexConstant.Htmlmark, "");
-                        ShowSummary(contents, out summary);
+                        summary = contents;
                     }
-                    ArticleHtml(articleId, title, crDate, typeName, summary);
+                    else
+                    {
+                        summary = contents.Substring(0, 100);
+                        summary = summary.Substring(0, 99) + System.Text.RegularExpressions.Regex.Replace(summary.Substring(99, 1), RegexConstant.AlphaNumber, "");
+                        summary += "......";
+                    }
                 }
+                Articleshtml += "<div class=\"row\">";
+                Articleshtml += "<div class=\"col-md-12 content-title\"><a href=\"ArticleDetail.aspx?id=" + articleId + "\">" + title + "</a></div>";
+                Articleshtml += "<div class=\"col-md-12 content-date\"><span>" + crDate + "</span><span>-</span><span>" + typeName + "</span></div>";
+                Articleshtml += "<div class=\"col-md-12 content-t\">" + summary;
+                Articleshtml += "</div>";
+                Articleshtml += "</div>";
             }
         }
 
-        public void ShowSummary(string contents,out string summary)
-        {
-            if (contents.Length < 100)
-            {
-                summary = contents;
-            }
-            else
-            {
-                summary = contents.Substring(0, 100);
-                summary = summary.Substring(0, 99) + System.Text.RegularExpressions.Regex.Replace(summary.Substring(99, 1), RegexConstant.AlphaNumber, "");
-                summary += "......";
-            }
-        }
-
-        public void ArticleHtml(int articleId, string title, string crDate, string typeName, string summary)
-        {
-            Articleshtml += "<div class=\"row\">";
-            Articleshtml += "<div class=\"col-md-12 content-title\"><a href=\"ArticleDetail.aspx?id=" + articleId + "\">" + title + "</a></div>";
-            Articleshtml += "<div class=\"col-md-12 content-date\"><span>" + crDate + "</span><span>-</span><span>" + typeName + "</span></div>";
-            Articleshtml += "<div class=\"col-md-12 content-t\">" + summary;
-            Articleshtml += "</div>";
-            Articleshtml += "</div>";
-        }
+        
 
     }
 }
