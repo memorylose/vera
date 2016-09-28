971dd7964f6bc2f20f9b575ac3e3e0cbb486052c
jiangyue1120 <jiangyue1120@126.com>
2016-09-28 21:23:27 +0800

    update

--- UM.UI/Index.aspx.cs
+++ UM.UI/Index.aspx.cs
@@ -14,35 +14,47 @@ namespace UM.UI
     {
         public string Pageheadhtml = "";
         public string RankListhtml = "";
-        //public string CrYearhtml = "";
-        //public string CrMonthhtml = "";
-        //public string CrDayhtml = "";
         public string ArticleDetailhtml = "";
         public string ArticleListhtml = "";
         protected void Page_Load(object sender, EventArgs e)
         {
             Pageheadhtml += "<div class=\"header-login\"><a href=\"../Register.aspx\">注册</a></div>";
             Pageheadhtml += "<div class=\"header-login\"><a href=\"../Login.aspx\">登录</a></div>";
+            ShowHotArticle();
+            ShowArticleList();
+        }
 
+        public void ShowHotArticle()
+        {
             UserRegisterBusiness userReg = new UserRegisterBusiness();
             DataSet ds = userReg.HotArticle();
             for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
             {
+                string title = ds.Tables[0].Rows[i]["Title"].ToString();
+                if (title.Length >= 17)
+                {
+                    title = title.Substring(0, 16);
+                    title += "...";
+                }
                 RankListhtml += "<div class=\"row bt-margin\">";
-                RankListhtml += "<div class=\"l-read-name\"><a href=\"ArticleDetail.aspx?id=" + Convert.ToInt32(ds.Tables[0].Rows[i]["ArticleId"]) + "\">" + ds.Tables[0].Rows[i]["Title"].ToString() + "</a></div>";
+                RankListhtml += "<div class=\"l-read-name\"><a href=\"ArticleDetail.aspx?id=" + Convert.ToInt32(ds.Tables[0].Rows[i]["ArticleId"]) + "\">" + title + "</a></div>";
                 RankListhtml += "<div class=\"l-read-name-d\">(239)</div>";
                 RankListhtml += "</div>";
             }
+        }
 
+        public void ShowArticleList()
+        {
+            UserRegisterBusiness userReg = new UserRegisterBusiness();
             DataSet ds1 = userReg.ShowArticle();
-            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            for (int i = 0; i < ds1.Tables[0].Rows.Count; i++)
             {
-                int articleId = Convert.ToInt32(ds.Tables[0].Rows[i]["ArticleId"]);
+                int articleId = Convert.ToInt32(ds1.Tables[0].Rows[i]["ArticleId"]);
                 string title = ds1.Tables[0].Rows[i]["Title"].ToString();
                 string crDate = ds1.Tables[0].Rows[i]["CreateDate"].ToString();
-                string crYear = crDate.Substring(0, 4).ToString();
-                string crMonth = crDate.Substring(5, 1).ToString();//我想日期格式是yyyy-mm-dd，这种在用户注册的时候能设置吗
-                string crDay = crDate.Substring(7, 2).ToString();
+                string crYear = DateTime.Parse(crDate).Year.ToString();
+                string crMonth = DateTime.Parse(crDate).Month.ToString();
+                string crDay = DateTime.Parse(crDate).Day.ToString();
                 string typeName = ds1.Tables[0].Rows[i]["TypeName"].ToString();
                 string summary = ds1.Tables[0].Rows[i]["Summary"].ToString();
                 if (summary == "")
@@ -60,8 +72,8 @@ namespace UM.UI
                         summary += "......";
                     }
                 }
-                ArticleListhtml+= "<div class=\"r-content-d\">";
-                ArticleListhtml+= "<div class=\"row row-marginb\">";
+                ArticleListhtml += "<div class=\"r-content-d\">";
+                ArticleListhtml += "<div class=\"row row-marginb\">";
                 ArticleListhtml += "<div class=\"col-md-1 bt-padding r-time-d\">";
                 ArticleListhtml += "<div class=\"r-date-t\">";
                 ArticleListhtml += "<div class=\"r-date-year\">" + crYear + "</div>";
@@ -73,18 +85,17 @@ namespace UM.UI
                 ArticleListhtml += "</div>";
                 ArticleListhtml += "<div class=\"col-md-10 bt-padding\">";
 
-                ArticleListhtml += "<div class=\"r-title\"><a href=\"Article/ArticleDetail.aspx?id=" + articleId+"\">" + title+"</a></div>";
-                ArticleListhtml += "<div class=\"r-time-1\"><a href=\"\">"+typeName+"</a></div>";
-                ArticleListhtml += "<div class=\"r-time-1\"><a href=\"Article/ArticleModify.aspx?id=" + articleId+"\">"+"编辑"+"</a></div>";
-                ArticleListhtml += "<div class=\"r-time-1\"><a href=\"Article/ArticleModify.aspx?id="+articleId+"\">"+"删除"+"</a></div>";
+                ArticleListhtml += "<div class=\"r-title\"><a href=\"Article/ArticleDetail.aspx?id=" + articleId + "\">" + title + "</a></div>";
+                ArticleListhtml += "<div class=\"r-time-1\"><a href=\"\">" + typeName + "</a></div>";
+                ArticleListhtml += "<div class=\"r-time-1\"><a href=\"Article/ArticleModify.aspx?id=" + articleId + "\">" + "编辑" + "</a></div>";
+                ArticleListhtml += "<div class=\"r-time-1\"><a href=\"Article/ArticleModify.aspx?id=" + articleId + "\">" + "删除" + "</a></div>";
                 ArticleListhtml += "<div style=\"clear: both\"></div>";
-                ArticleListhtml += "<div class=\"r-subject\">"+summary+"</div>";
+                ArticleListhtml += "<div class=\"r-subject\">" + summary + "</div>";
 
                 ArticleListhtml += "</div>";
                 ArticleListhtml += "</div>";
                 ArticleListhtml += "</div>";
             }
-
         }
     }
 }
