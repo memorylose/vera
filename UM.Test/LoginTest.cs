9811c45ee4ae328679bf73f86fe6a31caadf382d
memorylose <lucifering@126.com>
2016-10-10 15:35:15 +0800

    add unit test project

---
+++ UM.Test/LoginTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UM.BusinessLogic;
+
+namespace UM.Test
+{
+    /// <summary>
+    /// Summary description for LoginTest
+    /// </summary>
+    [TestClass]
+    public class LoginTest
+    {
+        public LoginTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestLoginValidationWithUsernameEmpty()
+        {
+            UserRegisterBusiness userReg = new UserRegisterBusiness();
+            string username = "";
+            string password = "";
+
+            string validateResult = userReg.LoginValidation(username, password);
+            Assert.AreEqual("User name is empty.", validateResult);
+        }
+    }
+}
