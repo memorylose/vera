190c2d86eca40164a2e1e6540a42145e9a30068d
jiangyue1120 <jiangyue1120@126.com>
2016-10-10 16:36:29 +0800

    update

---
+++ UM.Test/UnitTest2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UM.BusinessLogic;
+
+namespace UM.Test
+{
+    [TestClass]
+    public class LoginText1
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            UserRegisterBusiness useReg = new UserRegisterBusiness();
+            string username = "VeraJiang";
+            string password = "123";
+
+            string result = useReg.LoginValidation(username, password);
+            Assert.AreEqual("", result);
+        }
+    }
+}
