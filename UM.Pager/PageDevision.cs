58a7d049bab89fcf6aad9992db095d4a73090a82
jiangyue1120 <jiangyue1120@126.com>
2016-10-20 13:42:23 +0800

    update

--- UM.Pager/PageDevision.cs
+++ UM.Pager/PageDevision.cs
@@ -11,11 +11,9 @@ namespace UM.Pager
 {
     public class PageDevision : System.Web.UI.Page
     {
-        UserRegisterBusiness userReg = new UserRegisterBusiness();
-        DataSet ds;
-
-        public void DevidePage(int pageSize, string request, string link)
+        public StringBuilder DevidePage(int pageSize, string request, string link, out int beginRowNumber, out int endRowNumber)
         {
+            UserRegisterBusiness userReg = new UserRegisterBusiness();
             StringBuilder PageDevisionhtml = new StringBuilder();
             int articelNumber = userReg.CountNumber();
             int pageNumber = 0;
@@ -77,80 +75,10 @@ namespace UM.Pager
             }
             PageDevisionhtml.Append(link + nextPage.ToString() + "\">下一页</a>");
 
-            int beginRowNumber = (curPage - 1) * pageSize + 1;
-            int endRowNumber = curPage * pageSize;
-            ShowArtList(beginRowNumber, endRowNumber);
-        }
+            beginRowNumber = (curPage - 1) * pageSize + 1;
+            endRowNumber = curPage * pageSize;
 
-        public void ShowArtList(int beginRowNumber, int endRowNumber)
-        {
-            StringBuilder ArticleListhtml = new StringBuilder();
-            ds = userReg.ShowPageDevision(beginRowNumber, endRowNumber);
-            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
-            {
-                int articleId = Convert.ToInt32(ds.Tables[0].Rows[i]["ArticleId"]);
-                string title = ds.Tables[0].Rows[i]["Title"].ToString();
-                string crDate = ds.Tables[0].Rows[i]["CreateDate"].ToString();
-                string crYear = DateTime.Parse(crDate).Year.ToString();
-                string crMonth = DateTime.Parse(crDate).Month.ToString();
-                string month = userReg.ShowMonth(crMonth);
-                string crDay = DateTime.Parse(crDate).Day.ToString();
-                string typeName = ds.Tables[0].Rows[i]["TypeName"].ToString();
-                string summary = ds.Tables[0].Rows[i]["Summary"].ToString();
-                string author = ds.Tables[0].Rows[i]["UserName"].ToString();
-                string typeId = ds.Tables[0].Rows[i]["TypeId"].ToString();
-                if (summary == "")
-                {
-                    string contents = ds.Tables[0].Rows[i]["Contents"].ToString();
-                    contents = System.Text.RegularExpressions.Regex.Replace(contents, RegexConstant.Htmlmark, "");
-                    if (contents.Length < 100)
-                    {
-                        summary = contents;
-                    }
-                    else
-                    {
-                        summary = contents.Substring(0, 100);
-                        summary = summary.Substring(0, 99) + System.Text.RegularExpressions.Regex.Replace(summary.Substring(99, 1), RegexConstant.AlphaNumber, "");
-                        summary += "......";
-                    }
-                }
-                ArticleListhtml.Append("<div class=\"r-content-d\">");
-                ArticleListhtml.Append("<div class=\"row row-marginb\">");
-                ArticleListhtml.Append("<div class=\"col-md-1 bt-padding r-time-d\">");
-                ArticleListhtml.Append("<div class=\"r-date-t\">");
-                ArticleListhtml.Append("<div class=\"r-date-year\">" + crYear + "</div>");
-                ArticleListhtml.Append("<div class=\"r-date-month\">" + month + "</div>");
-                ArticleListhtml.Append("</div>");
-                ArticleListhtml.Append("<div class=\"r-date-b\">");
-                ArticleListhtml.Append(crDay);
-                ArticleListhtml.Append("</div>");
-                ArticleListhtml.Append("</div>");
-                ArticleListhtml.Append("<div class=\"col-md-10 bt-padding\">");
-                ArticleListhtml.Append("<div class=\"r-title\"><a href=\"Articles.aspx?id=" + articleId + "\">" + title + "</a></div>");
-                ArticleListhtml.Append("<div class=\"r-time-1\"><a href=\"Index.aspx?id=" + typeId + "\">" + typeName + "</a></div>");
-                if (Session["user"] != null)
-                {
-                    string username = Session["user"].ToString();
-                    if (username == author)
-                    {
-                        ArticleListhtml.Append("<div class=\"r-time-1\"><a href=\"Article/ArticleModify.aspx?id=" + articleId + "\">" + "编辑" + "</a></div>");
-                        ArticleListhtml.Append("<div class=\"r-time-1\"><a href=\"Article/ArticleModify.aspx?id=" + articleId + "\">" + "删除" + "</a></div>");
-                    }
-                    else
-                    {
-                        ArticleListhtml.Append("");
-                    }
-                }
-                else
-                {
-                    ArticleListhtml.Append("");
-                }
-                ArticleListhtml.Append("<div style=\"clear: both\"></div>");
-                ArticleListhtml.Append("<div class=\"r-subject\">" + summary + "</div>");
-                ArticleListhtml.Append("</div>");
-                ArticleListhtml.Append("</div>");
-                ArticleListhtml.Append("</div>");
-            }
+            return PageDevisionhtml;             
         }
     }
 }
--- UM.UI/Index.aspx.cs
+++ UM.UI/Index.aspx.cs
@@ -18,10 +18,13 @@ namespace UM.UI
         public void Page_Load(object sender, EventArgs e)
         {
             int pageSize = 10;
+            int beginRowNumber;
+            int endRowNumber;
             string request = Request.QueryString["pageid"];
             string link = "<a href=\"Index.aspx?pageid=";
             PageDevision pageDev = new PageDevision();
-            pageDev.DevidePage(pageSize, request, link);
+            PageDevisionhtml = pageDev.DevidePage(pageSize, request, link, out beginRowNumber, out endRowNumber);
+            ShowArtList(beginRowNumber, endRowNumber);
         }
 
 
@@ -135,7 +138,7 @@ namespace UM.UI
         //    {
         //        pageNumber = articelNumber / 10;
         //    }
-            
+
         //    if (Request.QueryString["pageid"] != null && userReg.ValidatePageId(Request.QueryString["pageid"]))
         //    {
         //        curPage = Convert.ToInt32(Request.QueryString["pageid"]);
@@ -186,75 +189,75 @@ namespace UM.UI
         //    ShowArtList(beginRowNumber, endRowNumber);
         //}
 
-        //public void ShowArtList(int beginRowNumber, int endRowNumber)
-        //{
-        //    ds = userReg.ShowPageDevision(beginRowNumber, endRowNumber);
-        //    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
-        //    {
-        //        int articleId = Convert.ToInt32(ds.Tables[0].Rows[i]["ArticleId"]);
-        //        string title = ds.Tables[0].Rows[i]["Title"].ToString();
-        //        string crDate = ds.Tables[0].Rows[i]["CreateDate"].ToString();
-        //        string crYear = DateTime.Parse(crDate).Year.ToString();
-        //        string crMonth = DateTime.Parse(crDate).Month.ToString();
-        //        string month = userReg.ShowMonth(crMonth);
-        //        string crDay = DateTime.Parse(crDate).Day.ToString();
-        //        string typeName = ds.Tables[0].Rows[i]["TypeName"].ToString();
-        //        string summary = ds.Tables[0].Rows[i]["Summary"].ToString();
-        //        string author = ds.Tables[0].Rows[i]["UserName"].ToString();
-        //        string typeId = ds.Tables[0].Rows[i]["TypeId"].ToString();
-        //        if (summary == "")
-        //        {
-        //            string contents = ds.Tables[0].Rows[i]["Contents"].ToString();
-        //            contents = System.Text.RegularExpressions.Regex.Replace(contents, RegexConstant.Htmlmark, "");
-        //            if (contents.Length < 100)
-        //            {
-        //                summary = contents;
-        //            }
-        //            else
-        //            {
-        //                summary = contents.Substring(0, 100);
-        //                summary = summary.Substring(0, 99) + System.Text.RegularExpressions.Regex.Replace(summary.Substring(99, 1), RegexConstant.AlphaNumber, "");
-        //                summary += "......";
-        //            }
-        //        }
-        //        ArticleListhtml.Append("<div class=\"r-content-d\">");
-        //        ArticleListhtml.Append("<div class=\"row row-marginb\">");
-        //        ArticleListhtml.Append("<div class=\"col-md-1 bt-padding r-time-d\">");
-        //        ArticleListhtml.Append("<div class=\"r-date-t\">");
-        //        ArticleListhtml.Append("<div class=\"r-date-year\">" + crYear + "</div>");
-        //        ArticleListhtml.Append("<div class=\"r-date-month\">" + month + "</div>");
-        //        ArticleListhtml.Append("</div>");
-        //        ArticleListhtml.Append("<div class=\"r-date-b\">");
-        //        ArticleListhtml.Append(crDay);
-        //        ArticleListhtml.Append("</div>");
-        //        ArticleListhtml.Append("</div>");
-        //        ArticleListhtml.Append("<div class=\"col-md-10 bt-padding\">");
-        //        ArticleListhtml.Append("<div class=\"r-title\"><a href=\"Articles.aspx?id=" + articleId + "\">" + title + "</a></div>");
-        //        ArticleListhtml.Append("<div class=\"r-time-1\"><a href=\"Index.aspx?id=" + typeId + "\">" + typeName + "</a></div>");
-        //        if (Session["user"] != null)
-        //        {
-        //            string username = Session["user"].ToString();
-        //            if (username == author)
-        //            {
-        //                ArticleListhtml.Append("<div class=\"r-time-1\"><a href=\"Article/ArticleModify.aspx?id=" + articleId + "\">" + "编辑" + "</a></div>");
-        //                ArticleListhtml.Append("<div class=\"r-time-1\"><a href=\"Article/ArticleModify.aspx?id=" + articleId + "\">" + "删除" + "</a></div>");
-        //            }
-        //            else
-        //            {
-        //                ArticleListhtml.Append("");
-        //            }
-        //        }
-        //        else
-        //        {
-        //            ArticleListhtml.Append("");
-        //        }
-        //        ArticleListhtml.Append("<div style=\"clear: both\"></div>");
-        //        ArticleListhtml.Append("<div class=\"r-subject\">" + summary + "</div>");
-        //        ArticleListhtml.Append("</div>");
-        //        ArticleListhtml.Append("</div>");
-        //        ArticleListhtml.Append("</div>");
-        //    }
-        //}
+        public void ShowArtList(int beginRowNumber, int endRowNumber)
+        {
+            ds = userReg.ShowPageDevision(beginRowNumber, endRowNumber);
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                int articleId = Convert.ToInt32(ds.Tables[0].Rows[i]["ArticleId"]);
+                string title = ds.Tables[0].Rows[i]["Title"].ToString();
+                string crDate = ds.Tables[0].Rows[i]["CreateDate"].ToString();
+                string crYear = DateTime.Parse(crDate).Year.ToString();
+                string crMonth = DateTime.Parse(crDate).Month.ToString();
+                string month = userReg.ShowMonth(crMonth);
+                string crDay = DateTime.Parse(crDate).Day.ToString();
+                string typeName = ds.Tables[0].Rows[i]["TypeName"].ToString();
+                string summary = ds.Tables[0].Rows[i]["Summary"].ToString();
+                string author = ds.Tables[0].Rows[i]["UserName"].ToString();
+                string typeId = ds.Tables[0].Rows[i]["TypeId"].ToString();
+                if (summary == "")
+                {
+                    string contents = ds.Tables[0].Rows[i]["Contents"].ToString();
+                    contents = System.Text.RegularExpressions.Regex.Replace(contents, RegexConstant.Htmlmark, "");
+                    if (contents.Length < 100)
+                    {
+                        summary = contents;
+                    }
+                    else
+                    {
+                        summary = contents.Substring(0, 100);
+                        summary = summary.Substring(0, 99) + System.Text.RegularExpressions.Regex.Replace(summary.Substring(99, 1), RegexConstant.AlphaNumber, "");
+                        summary += "......";
+                    }
+                }
+                ArticleListhtml.Append("<div class=\"r-content-d\">");
+                ArticleListhtml.Append("<div class=\"row row-marginb\">");
+                ArticleListhtml.Append("<div class=\"col-md-1 bt-padding r-time-d\">");
+                ArticleListhtml.Append("<div class=\"r-date-t\">");
+                ArticleListhtml.Append("<div class=\"r-date-year\">" + crYear + "</div>");
+                ArticleListhtml.Append("<div class=\"r-date-month\">" + month + "</div>");
+                ArticleListhtml.Append("</div>");
+                ArticleListhtml.Append("<div class=\"r-date-b\">");
+                ArticleListhtml.Append(crDay);
+                ArticleListhtml.Append("</div>");
+                ArticleListhtml.Append("</div>");
+                ArticleListhtml.Append("<div class=\"col-md-10 bt-padding\">");
+                ArticleListhtml.Append("<div class=\"r-title\"><a href=\"Articles.aspx?id=" + articleId + "\">" + title + "</a></div>");
+                ArticleListhtml.Append("<div class=\"r-time-1\"><a href=\"Index.aspx?id=" + typeId + "\">" + typeName + "</a></div>");
+                if (Session["user"] != null)
+                {
+                    string username = Session["user"].ToString();
+                    if (username == author)
+                    {
+                        ArticleListhtml.Append("<div class=\"r-time-1\"><a href=\"Article/ArticleModify.aspx?id=" + articleId + "\">" + "编辑" + "</a></div>");
+                        ArticleListhtml.Append("<div class=\"r-time-1\"><a href=\"Article/ArticleModify.aspx?id=" + articleId + "\">" + "删除" + "</a></div>");
+                    }
+                    else
+                    {
+                        ArticleListhtml.Append("");
+                    }
+                }
+                else
+                {
+                    ArticleListhtml.Append("");
+                }
+                ArticleListhtml.Append("<div style=\"clear: both\"></div>");
+                ArticleListhtml.Append("<div class=\"r-subject\">" + summary + "</div>");
+                ArticleListhtml.Append("</div>");
+                ArticleListhtml.Append("</div>");
+                ArticleListhtml.Append("</div>");
+            }
+        }
     }
 }
 
