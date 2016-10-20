2979473b616607347de533eb3b6822bf90a00705
memorylose <lucifering@126.com>
2016-10-20 15:41:48 +0800

    add interface project.

---
+++ UM.Interface/IUserRegister.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UM.Interface
+{
+    public interface IUserRegister
+    {
+        int UserLogin(string username, string password);
+    }
+}
